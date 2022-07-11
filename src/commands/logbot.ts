4824212dffd5648ac42d2ea95c63e3e7c8e4ebca
Brian Shao <dev@jzshao.com>
2022-07-11 11:28:03 -0700

    Clean up

--- src/commands/logbot.ts
+++ src/commands/logbot.ts
@@ -14,22 +14,24 @@ import { Discord, Slash, SlashGroup, ButtonComponent } from 'discordx';
 export class LogCommand {
   @Slash('create-log-message', { description: 'Create the log creation menu' })
   async createLogMessage(interaction: CommandInteraction): Promise<void> {
-    const { channel } = interaction;
     await interaction.reply({ content: 'Sending message...', ephemeral: true });
 
-    const embed = new MessageEmbed()
-      .setTitle('LogBot')
-      .setDescription('Use the button below to create a new log.');
-
-    const createBtn = new MessageButton()
-      .setLabel('New Log')
-      .setEmoji('➕')
-      .setStyle('PRIMARY')
-      .setCustomId('create-log');
-
-    const row = new MessageActionRow().addComponents(createBtn);
-
-    await channel?.send({ embeds: [embed], components: [row] });
+    await interaction.channel?.send({
+      embeds: [
+        new MessageEmbed()
+          .setTitle('LogBot')
+          .setDescription('Use the button below to create a new log.'),
+      ],
+      components: [
+        new MessageActionRow().addComponents(
+          new MessageButton()
+            .setLabel('New Log')
+            .setEmoji('➕')
+            .setStyle('PRIMARY')
+            .setCustomId('create-log')
+        ),
+      ],
+    });
 
     await interaction.editReply({ content: 'Message sent!' });
   }
